ebb01d8d2b14a5252c1a0d1635208f5696367a68
DmytroMalenko <malenkodmytro11@gmail.com>
2025-12-12 17:31:50 +0100

    initial commit

---
+++ HWpart2.py
@@ -0,0 +1,8 @@
+print("To be\nor not\nto be") # task 1
+print()
+print("''Life is what happens\n\t when\n\t you're busy making " \
+"other plans''\n\t John Lennon") # task 2
+print()
+print("''The best way to predict the future \n\tis" \
+" to\n\t\tinvent it.''\n\t\t\t Alan Kay") #task 3
+
---
+++ introduction.py
@@ -0,0 +1,39 @@
+r'''
+print("Hello World!")
+print(10,0,302,100)
+print("Hello","Dima!")
+
+print("Hello", end=" ")
+print("Dima.")
+
+print(10,0,302,100, sep=";")
+print("// - giuhgiub")
+print("/t - rivhh9e8")
+print("//n")
+print("/t/t some text")
+print(r"c:\Users\malen\Desktop\python_core")
+
+# kommentar
+
+'''
+r'''
+print(10 + 43)
+print(15 * 2 +5)
+print(10 / 13 +(12 + 4) / 5)
+
+print("10 + 12 = ", 10 + 12)
+print("10 - 12 = ", 10 - 12)
+print("10 * 12 = ", 10 * 12)
+print("10 / 12 = ", 10 / 12)
+print("10 % 12 = ", 10 % 12) # ostacha vid dillenya
+print("10 // 12 = ", 10 // 12) # cillochiselne dillenya
+print("10 ** 12 = ", 10 ** 12)
+
+print("-10", -10) # ynarne chislo
+print("+10", +10)
+
+from math import sqrt
+
+print(sqrt(9))
+'''
+ 
