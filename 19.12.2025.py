59af7ad07d0cb065d638e991fd13fef0045b7398
DmytroMalenko <malenkodmytro11@gmail.com>
2025-12-19 18:41:29 +0100

    initial commit

---
+++ 19.12.2025.py
@@ -0,0 +1,91 @@
+# Task 1
+'''
+number1 = float(input("Write first number:"))
+number2 = float( input("Write second number:"))
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+print(f"{number1} + {number2} = {number1 + number2}")
+print(f"{number1} - {number2} = {number1 - number2}")
+print(f"{number1} * {number2} = {number1 * number2}")
+
+
+
+# Task 2
+
+number1 = float(input("Write first number:"))
+number2 = float(input("Write second number:"))
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+print(f"{number1} / {number2} = {number1 / number2}") 
+
+
+
+# Task 3
+
+number1 = float(input("Write first number:"))
+number2 = float(input("Write second number:"))
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+print(f"{number1} * {number2} = {number1 * number2}") 
+
+
+# Task 4
+
+print("P = 4x a")
+number1 = float(input("Write first number:"))
+number2 = float(input("Write second number:"))
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+print(f"{number1} * {number2} = {number1 * number2}")
+
+
+
+# Task 5
+
+print("1m = 100cm")
+number1 = float(input("Write first number:"))
+
+number1 = int(number1)
+
+print(type(number1))
+
+
+print(f"{number1} * 100 = {number1 * 100}","cm")
+
+'''
+
+# Task 6
+
+print("How much liter of paint do you need?")
+number1 = float(input("Write how much do you need:"))
+number2 = float(input("Write how many wall do you need to paint:"))
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+print(f"{number1} * {number2} = {number1 * number2}")
+
+print(" You need", f"{number1 * number2}.")
---
+++ data.types.py
@@ -0,0 +1,110 @@
+# tupu dannih
+'''
+print("Hello python!")
+print(150)
+print(100 + 150)
+
+# Literal - znachennya scho vkasyetcya bezposeredno v codi
+# Operator - spezialnui simbol, scho vukonye pevny operaciq
+#
+#
+
+print(150 + 10 -(150 + 1) * 145)
+print(150 / 5)
+
+ # print("hello / world!")
+ # str (string) - text, pevna poslidovnist symbola
+ # int (integer) - cili chisla
+ # float (floating point number) - chisla z plavachqq tochkoq, drobovi chisla 12,5, 14,7, -7,567, -8.3, 5.8
+
+print("Hello") # str
+print(-100) # int
+print(5.6) #float
+print(100 + 45) # int
+print(100 / 5) #float
+print("Hello" + "world") # konkatenachiya ryadkiv
+print("qwerty" * 5)
+
+print(type(-100))
+print(type(15 / 3))
+print(type("some text"))
+'''
+
+name = "Dima"
+age = 16
+
+'''
+# print(" Your name: " + name)
+# print(" Your age: ", age)
+
+# print("name")
+# print(name)
+print(f"Your name: {name}")
+print(f"Your age: {age}")
+print(f"10 + 5 = {10 + 5}")
+
+result = 10 + 16
+print(f"10 + 16 = {result}")
+
+name = "Anton"
+age = 18
+
+print(f"Your name: {name}")
+print(f"Your age: {age}")
+
+user1 = "User 1"
+user_1 = "user_1"
+zminna = "text"
+# zminna = "text"
+# 1user = "1 user" - ERROR
+
+userid = 123
+UserId = 1234
+userId = 12345
+user_id = 123456
+'''
+'''
+number = 10
+print(type(number))
+print(number)
+# number = number + 5
+number += 5
+print(number)
+
+number -= 5 # number = number - 5
+number *= 5 # number = number * 5
+number /= 5 # number = number / 5
+number %= 5 # number = number % 5
+number //= 5 # number = number // 5
+number **= 5 # number = number ** 5
+
+print(type(number))
+
+number = "100"
+print(number)
+print(type(number))
+
+'''
+'''
+user_age = input("Write your name: ")
+user_name = input("Write your age: ")
+
+print(f"Your Name: {user_name}")
+print(f"Your Age: {user_age}")
+'''
+number1 = float(input("Write first number:"))
+number2 = float(input("Write second number:"))
+
+# number1 = int(number1)
+# number2 = float(number2)
+
+number1 = int(number1)
+number2 = float(number2)
+
+print(type(number1))
+print(type(number2))
+
+# 14 + 15
+# "14" + "15"
+
+print(f"{number1} / {number2} = {number1 / number2}")
