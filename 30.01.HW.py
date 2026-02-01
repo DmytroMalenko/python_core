52197087b8e30b5213ef5aef2e1259bb2b3746db
DmytroMalenko <malenkodmytro11@gmail.com>
2026-02-01 12:02:18 +0100

    initial commit

---
+++ 30.01.HW.py
@@ -0,0 +1,90 @@
+while True:
+    print("MENU")
+    print("First figure -   a")
+    print("Second figure -  б")
+    print("Third figure -   в")
+    print("Fourth figure -  г")
+    print("Fifth figure -   д")
+    print("Sixth figure -   е")
+    print("Seventh figure - ж")
+    print("Eight figure -   з")
+    print("Ninth figure -   и")
+    print("Tenth figure -   к")
+    break
+
+chose = str(input("Write figure (a -  к): "))
+if chose == 'a':
+    print("*****")
+    print(" ****")
+    print("  ***")
+    print("   **")
+    print("    *")
+elif chose == 'б':
+    print("*    ")
+    print("**   ")
+    print("***  ")
+    print("**** ")
+    print("*****")
+elif chose == 'в':
+    print("******")
+    print(" **** ")
+    print("  **  ")
+elif chose == 'г':
+    print("  **  ")
+    print(" **** ")
+    print("******")
+elif chose == 'д':
+    print("*******")
+    print(" ***** ")
+    print("  ***  ")
+    print("   *   ")
+    print("   *   ")
+    print("  ***  ")
+    print(" ***** ")
+    print("*******")
+elif chose == 'е':
+    print("*         *")
+    print("* *     * *")
+    print("* * * * * *")
+    print("* *     * *")
+    print("*         *")
+elif chose == 'ж': 
+    print("*    ")
+    print("* *  ")
+    print("* * *")
+    print("* *  ")
+    print("*    ")
+elif chose == 'з':
+    print("       *")
+    print("     * *")
+    print("   * * *")
+    print("     * *")
+    print("       *")
+elif chose == 'и': 
+    print("*****")
+    print("****")
+    print("***")
+    print("**")
+    print("*")
+elif chose == 'к':
+    print("      *")
+    print("     **")
+    print("    ***")
+    print("   ****")
+    print("  *****")
+else:
+    print("Erorr, write a - k.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
